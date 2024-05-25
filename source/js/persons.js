89e2c4993f96550436222d86dd68ca4db5ae4322
DimitrusB <dimitrusboss@mail.ru>
2024-05-25 23:33:48 +0300

    fix

--- source/js/persons.js
+++ source/js/persons.js
@@ -2,53 +2,43 @@ import { person } from "./const.js";
 
 const aboutPersons = document.getElementById("persons");
 let currentIndex = 0;
-console.log(person);
-renderPerson(person[currentIndex]);
 
-function renderPerson(personInfo) {
-  const card = document.createElement("div");
-  card.classList.add("person__item");
-  card.innerHTML = `
-    <div style="display: flex; justify-content: space-between; width: 100%;">
-      <img src="../../img/line.svg" alt="prev" id="prev">
-      <img src="${personInfo.image}" alt="${personInfo.name}">
-      <img src="../../img/line.svg" alt="next" style="transform: rotate(180deg);" id="next">
-    </div>
-    <div>
-    <div class="person__item_name">${personInfo.name}</div>
-    <div class="person__item_specialization">${personInfo.specialization}</div>
-  </div>
-  <ul class="person__experience">
-    ${personInfo.experience
-      .map((exp) => `<li class="person__item_experience">${exp}</li>`)
-      .join("")}
-  </ul>
-`;
-aboutPersons.appendChild(card);
+renderPerson(currentIndex);
+
+function renderPerson(index) {
+    const personInfo = person[index];
+    const card = document.createElement("div");
+    card.classList.add("person__item");
+    card.innerHTML = `
+        <div style="display: flex; justify-content: space-between; width: 100%;">
+            <img src="../../img/line.svg" alt="prev" class="prev">
+            <img src="${personInfo.image}" alt="${personInfo.name}">
+            <img src="../../img/line.svg" alt="next" style="transform: rotate(180deg);" class="next">
+        </div>
+        <div>
+            <div class="person__item_name">${personInfo.name}</div>
+            <div class="person__item_specialization">${personInfo.specialization}</div>
+        </div>
+        <ul class="person__experience">
+            ${personInfo.experience.map((exp) => `<li class="person__item_experience">${exp}</li>`).join("")}
+        </ul>
+    `;
+    aboutPersons.innerHTML = ''; 
+    aboutPersons.appendChild(card);
+
+    const prev = card.querySelector(".prev");
+    prev.addEventListener("click", () => {
+        if (currentIndex > 0) {
+            currentIndex -= 1;
+            renderPerson(currentIndex);
+        }
+    });
+
+    const next = card.querySelector(".next");
+    next.addEventListener("click", () => {
+        if (currentIndex < person.length - 1) {
+            currentIndex += 1;
+            renderPerson(currentIndex);
+        }
+    });
 }
-
-const prev = document.getElementById("prev");
-
-prev.addEventListener("click", () => {
-    next.disabled = false;
-  if (currentIndex > 0) {
-    currentIndex -= 1;
-    aboutPersons.innerHTML = "";
-    renderPerson(person[currentIndex]);
-  } else {
-    prev.disabled = true;
-  }
-});
-
-const next = document.getElementById("next");
-next.addEventListener("click", () => {
-    prev.disabled = false;
-  if (currentIndex < person.length -1 ) {
-    currentIndex += 1;
-    console.log(currentIndex);
-    aboutPersons.innerHTML = "";
-    renderPerson(person[currentIndex]);
-  } else {
-    next.disabled = true;
-  }
-});
